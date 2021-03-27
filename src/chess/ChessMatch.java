d7b6ab552a081e54a5932ceb831050a91f7b13dc
anderson-indiano <indianobox2@gmail.com>
2021-03-27 18:34:14 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,12 @@ public class ChessMatch {
 		return matrizPiece;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
@@ -65,37 +71,37 @@ public class ChessMatch {
 
 	private void initalSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
-		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
-		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
+//		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+//		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+//		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
-		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
-		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+//		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+//		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
+//		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
-		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
-		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
+//		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
+//		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+//		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
-		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+//		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+//		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+//		placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
 	}
 }
