261b2727b97b290113d1503e2c065c708a3dfcec
anderson-indiano <indianobox2@gmail.com>
2021-03-31 18:20:11 -0300

    Check logic

--- src/application/UserInterface.java
+++ src/application/UserInterface.java
@@ -54,6 +54,10 @@ public class UserInterface {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("waiting player: " + chessMatch.getCurrentPlayer());
+		
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
